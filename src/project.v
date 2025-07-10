50db25fcaaa26f6fc9fe1c313ce0a8748bb558ba
Winston Li <winston.h.li@sjsu.edu>
2025-07-10 03:33:05 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Winston Li
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -33,6 +33,5 @@ module tt_um_winston_prbs (
         
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+    wire _unused = &{ena, uio_in, ui_in, 1'b0};
 endmodule
